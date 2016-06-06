aa386000c0b39af87beab91c46967e715bb25918
Thomas Kerpe <thomas.kerpe@1und1.de>
2016-06-06 23:22:47 +0200

    Added some additional multimedia commands

--- bash/.bash/multimedia.sh
+++ bash/.bash/multimedia.sh
@@ -21,3 +21,10 @@ function mixer-mute {
     "${MIXER_BIN}" -t
 }
 
+alias mm_pause="xdotool key XF86AudioPlay"
+alias mm_prev="xdotool key XF86AudioPrev"
+alias mm_next="xdotool key XF86AudioNext"
+alias mm_up="xdotool key XF86AudioLowerVolume"
+alias mm_down="xdotool key XF86AudioRaiseVolume"
+alias mm_mute="xdotool key XF86AudioMute"
+
