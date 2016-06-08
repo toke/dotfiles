e40e7e5be652be4b312d796ec30b7709cac93747
Thomas Kerpe <thomas.kerpe@1und1.de>
2016-06-08 19:56:54 +0200

    [BASH] Added srv-cssh

--- bash/.bash/helper.bash
+++ bash/.bash/helper.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Helper for allowing only aliases to existing commands
 # Similar to [ -e /usr/bin/translate-shell ] && alias abc="/usr/bin/abc"
@@ -14,4 +15,21 @@ function bin_alias {
     command -v "$aliascontent" > /dev/null && \
     		alias $aliasname="$aliascontent"
   fi
+}
+
+function srv-cssh {
+    array=(${1//@/ })
+    if [[ -z ${array[0]} ]] ; then
+        echo "Usage: srv-cssh [user@]host"
+        return 1
+    elif [[ -z ${array[1]} ]] ; then
+        host=${array[0]}
+        user=""
+    else
+        host=${array[1]}
+        user="-l ${array[0]}"
+    fi
+
+    dns="" # dig syntax @ns
+    $(dig +short -t SRV _ssh._tcp.${host} ${dns} | awk -v USER="${user}" -e 'BEGIN{ printf "cssh " USER} { printf  " " $4;} END {print "";}')
 }
